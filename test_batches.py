a177984b21e7ac39ea0a774088a1e8e95409a322
gordonzu <gordonzu@outlook.com>
2020-03-14 17:14:15 -0400

    Finish initial domain model tests

--- test_batches.py
+++ test_batches.py
@@ -1,23 +1,21 @@
-from datetime import date, timedelta
 import pytest
+from datetime import date, timedelta
 from model import OrderLine, Batch
 
 today = date.today()
 tomorrow = today + timedelta(days=1)
 later = tomorrow + timedelta(days=10)
 
-def test_allocating_to_a_batch_reduces_the_available_quantity():
-    batch = Batch("batch-001", "SMALL-TABLE", qty=20, eta=date.today())
-    line = OrderLine('order-ref', "SMALL-TABLE", 2)
-
-    batch.allocate(line)
-    assert batch.available_quantity == 18
-
 def make_batch_and_line(sku, batch_qty, line_qty):
     return (
         Batch("batch-001", sku, batch_qty, eta=date.today()),
         OrderLine("order-123", sku, line_qty)
     )
+
+def test_allocating_to_a_batch_reduces_the_available_quantity():
+    batch, line = make_batch_and_line("ELEGANT-LAMP", 20, 2)
+    batch.allocate(line)
+    assert batch.available_quantity == 18
 
 def test_can_allocate_if_available_greater_than_required():
     large_batch, small_line = make_batch_and_line("ELEGANT-LAMP", 20, 2)
@@ -36,23 +34,22 @@ def test_cannot_allocate_if_skus_do_not_match():
     different_sku_line = OrderLine("order-123", "EXPENSIVE-TOASTER", 10)
     assert batch.can_allocate(different_sku_line) is False
 
+def test_cannot_deallocate_non_allocated_lines():
+    batch, unallocated_line = make_batch_and_line("DECORATIVE-TRINKET", 20, 2)
+    batch.deallocate(unallocated_line)
+    assert batch.available_quantity == 20
 
+def test_can_deallocate_allocated_lines():
+    batch, line = make_batch_and_line("ELEGANT-LAMP", 20, 2)
+    batch.allocate(line)
+    batch.deallocate(line)
+    assert batch.available_quantity == 20
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def test_allocation_is_idempotent():
+    batch, line = make_batch_and_line("ANGULAR-DESK", 20, 2)
+    batch.allocate(line)
+    batch.allocate(line)
+    assert batch.available_quantity == 18
 
 
 
